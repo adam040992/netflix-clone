95696f7cf06157171af38e36410db52235c7d016
adam040992 <adam040992@gmail.com>
2021-03-20 09:32:46 +0100

    Creating Netflix clone - Start

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA5wyNxYZOg6HHTuiKEhg23LrIQAD6w-zE",
+    authDomain: "netflix-clone-4c0e0.firebaseapp.com",
+    projectId: "netflix-clone-4c0e0",
+    storageBucket: "netflix-clone-4c0e0.appspot.com",
+    messagingSenderId: "900964338786",
+    appId: "1:900964338786:web:f361f44da311ba757cd53b"
+  };
