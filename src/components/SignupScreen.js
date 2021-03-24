b9b2ef9df3742a2b05a57d13d548703306e9f8dd
adam040992 <adam040992@gmail.com>
2021-03-24 18:14:19 +0100

    Setting up Athentication

--- src/components/SignupScreen.js
+++ src/components/SignupScreen.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { auth } from '../firebase';
 import './SignupScreen.scss';
 
 function SignInScreen() {
+
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+
+    const register = (e) => {
+        e.preventDefault();
+
+        auth.createUserWithEmailAndPassword(
+            emailRef.current.value,
+            passwordRef.current.value
+        )
+        .then((authUser) => {
+            console.log(authUser);
+        })
+        .catch((error) => {
+            alert(error.message);
+        });
+    };
+
+    const SignIn = (e) => {
+        e.preventDefault();
+    }
+
     return (
-        <div className="signInScreen">
-            <h1>not signet</h1>
+        <div className="signupScreen">
+            <form>
+                <h1>Sign In</h1>
+                <input ref={emailRef} placeholder="Email" type="email" />
+                <input ref={passwordRef} placeholder="Password" type="password" />
+                <button type="submit" onClick={SignIn}>Sign In</button>
+
+                <h4>
+                    <span className="signupScreen__gray">New to Netflix? </span>
+                    <span onClick={register} className="signupScreen__link">Sign Up now.</span>
+                </h4>
+            </form> 
         </div>
     )
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyA5wyNxYZOg6HHTuiKEhg23LrIQAD6w-zE",
     authDomain: "netflix-clone-4c0e0.firebaseapp.com",
@@ -6,3 +8,10 @@ const firebaseConfig = {
     messagingSenderId: "900964338786",
     appId: "1:900964338786:web:f361f44da311ba757cd53b"
   };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
