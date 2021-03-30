7493842524ff4f4b7d0f1cddd8ef65ae709a628f
adam040992 <adam040992@gmail.com>
2021-03-30 19:03:35 +0200

    profileScreen building - almost finished

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -27,6 +27,7 @@ function Nav() {
         <div className={`nav ${show && "nav__black"}`}>
             <div className="nav__contents">
                 <img 
+                    onClick={() => history.push('/')}
                     className="nav__logo"
                     src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" 
                     alt="Netflix Logo" 
--- src/components/ProfileScreen.js
+++ src/components/ProfileScreen.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import './ProfileScreen.scss';
+import Nav from './Nav';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../features/userSlice';
+import { auth } from '../firebase'; 
 
 function ProfileScreen() {
+    const user = useSelector(selectUser)
+
     return (
         <div className="profileScreen">
-            <h1>This is the profile</h1>
+            <Nav />
+            <div className="profileScreen__body">
+                <h1>Edit Profile</h1>
+                <div className="profileScreen__info">
+                    <img src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt="Avatar" />
+                    <div className="profileScreen__details">
+                        <h2>{user.email}</h2>
+                        <div className="profileScreen__plans">
+                            <h3>Plans</h3>
+                            <p>
+                                
+                            </p>
+                        <button 
+                            onClick={() => auth.signOut()}
+                            className="profileScreen__signOut"
+                        >
+                            Sign Out
+                        </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
